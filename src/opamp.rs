0679cbe4f664c04a02efeae4ccaa1eed13c85d37
Albin Hedman <albin.hedman@dectron.se>
2023-08-25 17:08:47 +0200

    Fix bug in opamp

--- src/opamp.rs
+++ src/opamp.rs
@@ -347,7 +347,7 @@ macro_rules! opamps {
                 ) -> (
                     $($opamp::Disabled,)*
                 ) {
-                    rcc.rb.apb2enr.write(|w| w.syscfgen().set_bit());
+                    rcc.rb.apb2enr.modify(|_, w| w.syscfgen().set_bit());
 
                     (
                         $($opamp::Disabled,)*
